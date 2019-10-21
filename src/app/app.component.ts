8c8b3b7c108d7e77b568481ad2e3ce7510938dd3
Olivia Cobble <ocobble@hotmail.com>
2019-10-21 12:42:20 -0500

    Started working on ninjafy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,33 @@ export class AppComponent {
   firstName = 'Olivia';
   lastName = 'Cobble';
   hogwartsHouse = 'wut';
-
+  ninjaSounds = ["ka", "zu", "mi", "te", "ku", "lu", 
+            "ji", "ri", "ki", "zus", "me", "ta", "rin", "to", "mo", "no", "ke", 
+            "shi", "ari", "chi", "ha", "ru", "mei", "na", "fu", "zi"];
+  ninjaName = 'mystery';
   buttonClick() {
     console.log("Ninjafy!");
   }
+
+  
+
+    ninjafy(name:string) {
+        var ninjyName:string = "";
+        
+        if (name.length == 0)
+        {
+        	return ninjyName;
+        }
+        
+        var index = 0;
+        while (index < 3 && index < name.length) { // You can change this!
+            var letter = name.charAt(index);
+            letter = letter.toLowerCase();
+            ninjyName += this.ninjaSounds[letter.charCodeAt(0) - 10];
+        }
+        
+        ninjyName = ninjyName.charAt(0).toUpperCase() + ninjyName.substring(1, ninjyName.length);
+        
+        return ninjyName;
+    }
 }

4b6a254bd5486ab6a6316fbddc8421ac2ee7f878
Olivia Cobble <ocobble@hotmail.com>
2019-10-24 10:08:00 -0500

    Added village questions

--- src/app/village/village.component.ts
+++ src/app/village/village.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VillageComponent implements OnInit {
 
+  q1 = -1;
+  q2 = -1;
+  q3 = -1;
+  q4 = -1;
+  q5 = -1;
+  q6 = -1;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  buttonclick() {
+    console.log("Value of hog is " + this.q1);
+    console.log("Value of run is " + this.q2);
+    console.log("Value of book is " + this.q3);
+  }
 }
